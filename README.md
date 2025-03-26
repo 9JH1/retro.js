7f4bf3325afa6ccb40d2e6294329ff1244667661
9JH1 <tkf.x1os@gmail.com>
2025-03-27 10:11:40 +1300

    added online support and readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# `RETRO.js`
+> Light-weight theme loader with a styled 90's look.
+![image](/src/img/2025-03-27_10-00-58.png)
+## Quickstart
+To install retro.js simply place this inside your html pages head element.
+```html
+<script src="https://github.com/retro.js/retro.js"></script>
+```
+## Install Locally 
+```bash
+git clone https://github.com/retro.js/
+git cd retro.js
+mv * ../
+```
--- retro.js
+++ retro.js
@@ -1,17 +1,32 @@
-const theme_t = "lime";
-const theme_l = "light";
+var theme_t = "lime";
+var theme_l = "light";
+var retro=true;
+var quickstart=true;
 
+
+
+(()=>{
+    if(quickstart){
+        quickstart = "https://raw.github.com/"
+    }
+})();
 (() => { // import theme
-    const theme = document.createElement("link");
-    theme.rel = "stylesheet";
-    theme.href = `/src/css/themes/${theme_t}.css`;
-    document.head.append(theme)
+    if(theme_t !=false){
+        const theme = document.createElement("link");
+        theme.rel = "stylesheet";
+        theme.href = `/src/css/themes/${theme_t}.css`;
+        theme.id = "retro-theme";
+        document.head.append(theme)
+    }
 })();
 (() => { // import css
-    const retroCss = document.createElement("link");
-    retroCss.rel = "stylesheet";
-    retroCss.href = "src/css/retro.css";
-    document.head.append(retroCss)
+    if(retro){
+        const retroCss = document.createElement("link");
+        retroCss.rel = "stylesheet";
+        retroCss.href = "src/css/retro.css";
+        retroCss.id="retro-css";
+        document.head.append(retroCss)
+    }
 })();
 (() => { // set theme dark/light
     if (theme_l == "dark") {
@@ -19,7 +34,7 @@ const theme_l = "light";
             document.body.classList.toggle('dark');
         }
     }
-})()
+})();
 
 function getTotalPageSize() {
     let totalSize = 0;
@@ -34,3 +49,23 @@ function getTotalPageSize() {
     return (totalSize / 1024).toFixed(2);
 }
 const size = getTotalPageSize();
+
+
+function resizeText() {
+    document.querySelectorAll(".dropdown a").forEach(text => {
+      const container = text.parentElement; // The .dropdown container
+      let fontSize = 100; // Start large
+      text.style.fontSize = `${fontSize}px`;
+  
+      // Decrease font size until it fits inside the container
+      while (text.scrollWidth > container.clientWidth || text.scrollHeight > container.clientHeight) {
+        fontSize--;
+        text.style.fontSize = `${fontSize}px`;
+      }
+    });
+  }
+  
+  // Run on load and window resize
+  window.addEventListener("resize", resizeText);
+  resizeText();
+  
